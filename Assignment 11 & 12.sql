c2cdad8cb22dfb70ffc7e331640930383247092b
leenamahajan1707 <65151780+leenamahajan1707@users.noreply.github.com>
2020-07-26 19:20:35 +0530

    Update Assignment 11 & 12.sql

--- Assignment 11 & 12.sql
+++ Assignment 11 & 12.sql
@@ -13,7 +13,9 @@ select * from salespeople;
 
 -- 2) Write a query that produces the names and ratings of all customers who have
 -- above-average orders.
-	select cname,rating from customers where rating > (select Avg(rating) from customers);
+	select cname,rating from customers where cnum IN
+    	(select cnum from orders where amt >
+	(select avg(amt) from orders));
 	
 -- 3) Write a query that selects the total amount in orders for each salesperson for
 -- whom this total is greater than the amount of the largest order in the table.
